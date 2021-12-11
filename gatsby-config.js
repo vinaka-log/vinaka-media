dd19db73f33d726a399fcd9d76a2cfd628f5c6bb
ky__yk <vinaka@ky-pro.local>
2021-12-11 15:50:35 +0900

    first commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -31,8 +31,25 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
+    {
+      resolve: "gatsby-source-microcms",
+      options: {
+        apiKey: process.env.API_KEY,
+        serviceId: 'vinaka-blod-media',
+        apis: [
+          {
+            endpoint: "blog",
+          },
+        ],
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
   ],
 }
+const path = require('path');
+
+require('dotenv').config({
+  path: `.env.${process.env.NODE_ENV}`
+});
